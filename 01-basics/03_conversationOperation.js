0b673100271607c8f3fbd7cfd24b739bae5518c9
jaiminpanchal222 <86374840+jaiminpanchal222@users.noreply.github.com>
2023-01-25 09:58:36 +0000

    conversationOperation

---
+++ 01-basics/03_conversationOperation.js
@@ -0,0 +1,26 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInnumber = Number(score);
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//33 => 33 is convertable
+//"33abc" =>  Nan Not convertable means not convertable
+//true => converting in number 1 and 
+//false => converting in number 0.
+
+let isLoggedIn = "Jaimin";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// if you write 1 => true in boolean; if you write 0 => false in boolean;
+//"" => false
+//"Jaimin =>true"
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
